acfa96575dafd33d5c32c3542df185df5da47485
Erik.Knoos <erik.knoos@stud.sti.se>
2025-06-10 18:38:08 +0200

    ADD:Player test, test to see if hit works

---
+++ src/test/java/blackjack/model/PlayerTest.java
@@ -0,0 +1,28 @@
+package blackjack.model;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PlayerTest {
+
+    @Test
+    public void testInitialWantsToHitTrue() {
+        Player player = new Player();
+        assertTrue(player.wantsToHit());
+    }
+
+    @Test
+    public void testSetWantsToHitFalse() {
+        Player player = new Player();
+        player.setWantsToHit(false);
+        assertFalse(player.wantsToHit());
+    }
+
+    @Test
+    public void testReceiveCardAddsToHand() {
+        Player player = new Player();
+        player.receiveCard(new Card(Card.Suit.SPADES, Card.Rank.EIGHT));
+        assertEquals(1, player.getHand().getCards().size());
+    }
+}
+
