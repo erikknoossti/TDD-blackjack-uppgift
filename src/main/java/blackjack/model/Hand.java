98423ef6585c9506f307cfddfbeedf1b523c1159
Erik.Knoos <erik.knoos@stud.sti.se>
2025-06-10 18:13:07 +0200

    ADD: some logic, method to calculate score

--- src/main/java/blackjack/model/Hand.java
+++ src/main/java/blackjack/model/Hand.java
@@ -1,7 +1,36 @@
 package blackjack.model;
 
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class Hand {
+    private final List<Card> cards = new ArrayList<>();
 
+    public void addCard(Card card) {
+        cards.add(card);
+    }
+
+    public List<Card> getCards() {
+        return cards;
+    }
+
+    public int getScore() {
+        int total = 0;
+        int aceCount = 0;
+
+        for (Card card : cards) {
+            total += card.getValue();
+            if (card.getRank() == Card.Rank.ACE) {
+                aceCount++;
+            }
+        }
+
+        // Om total > 21, då går A från 11 till 1
+        while (total > 21 && aceCount > 0) {
+            total -= 10;
+            aceCount--;
+        }
+
+        return total;
+    }
 }
